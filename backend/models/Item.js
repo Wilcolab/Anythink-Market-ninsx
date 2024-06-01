98031b69c36cc3c048100193c41293513e7604fb
İrem Akyol <69118634+lastirembender@users.noreply.github.com>
2024-06-01 22:33:18 +0300

    Update Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var uniqueValidator = require("mongoose-unique-validator");
 var slug = require("slug");
 var User = mongoose.model("User");
-import placeholder from 'backend/models/placeholder.png'
+var placeholder = '/placeholder.png'
 
 var ItemSchema = new mongoose.Schema(
   {
